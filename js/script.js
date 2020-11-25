bf4ca52871a83f71fc9fba19ef8104159c9319f5
unknown <1224061385@qq.com>
2020-11-25 20:13:39 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,119 @@
+(function ($) {
+  'use strict';
+
+
+  // bottom to top
+  $('#top').click(function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 'slow');
+    return false;
+  });
+  // bottom to top
+
+  $(document).on('ready', function () {
+
+    // Nice Select
+    $('select').niceSelect();
+    // -----------------------------
+    //  Client Slider
+    // -----------------------------
+    $('.category-slider').slick({
+      slidesToShow: 8,
+      infinite: true,
+      arrows: false,
+      autoplay: false,
+      autoplaySpeed: 2000
+    });
+
+    // -----------------------------
+    // Coupon Type Select
+    // -----------------------------
+    $('#online-code').click(function () {
+      $('.code-input').fadeIn(500);
+    });
+    $('#store-coupon, #online-sale').click(function () {
+      $('.code-input').fadeOut(500);
+    });
+    /***ON-LOAD***/
+    jQuery(window).on('load', function () {
+
+    });
+
+  });
+
+  // niceSelect
+
+  $('select:not(.ignore)').niceSelect();
+
+
+
+
+  //轮播图
+
+  $('.trending-ads-slide').slick({
+    dots: false,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 800,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+
+    ]
+  });
+
+  // product-slider
+  $('.product-slider').slick({
+    dots: true,
+    arrows: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: false,
+    nextArrow: '<i class="fa fa-chevron-right arrow-right"></i>',
+    prevArrow: '<i class="fa fa-chevron-left arrow-left"></i>',
+    customPaging: function (slider, i) {
+      var image = $(slider.$slides[i]).data('image');
+      return '<img class="img-fluid" src="' +  + '" alt="">';
+    }
+  });
+
+  // tooltip
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+     // bootstrap slider range
+  $('.range-track').slider({});
+  $('.range-track').on('slide', function (slideEvt) {
+    $('.value').text('$' + slideEvt.value[0] + ' - ' + '$' + slideEvt.value[1]);
+  });
+
+
+})(jQuery);
+
+/*购物车界面*/
+
